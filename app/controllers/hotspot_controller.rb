2cba523d0e9e48bf0f7d1fb8bf0a0a9925c20989
Simone Bravo <simonebravo96@gmail.com>
2015-07-29 18:44:48 -0400

    Working on views

--- app/controllers/hotspot_controller.rb
+++ app/controllers/hotspot_controller.rb
@@ -1,12 +1,51 @@
-# Ruby version oh the chillispot login page.
-# 14/07/2015 Bravo Simone.
+# Ruby implementation of the chillispot hotspotlogin page
+# Author: Bravo Simone
+# Date: 15/07/15
 
-class HotspotController < ApplicationController
+def index
+end
 
-  def index
+def login
+  # Basic variables definitiona
+  # Comment this if you don't use the uamsecret
+  $uamsecret = "sharedkey"
+  # This variable contains the state of the client
+  @case = params['res']
+end
+
+# Login function
+def login
+  chal = params['chal']
+  password = params['password']
+  $uamsecret = "Secreto"
+  @hexchal = Array[chal].pack("H32")
+  if $uamsecret
+    newchal = Array[Digest::MD5.hexdigest(@hexchal + $uamsecret)].pack("H*")
+  else
+    newchal = @hexchal
   end
+  response = Digest::MD5.hexdigest("\0" + password + newchal)
+  newpwd = Array[password].pack("a32")
+  pappassword = newpwd.xor(newchal).unpack("H32").first
+end
 
-  def login
-    @user = Hotspot.koala(request.env['omniauth.auth']['credentials'])
-  end
+def notyet
+end
+
+def faied
+end
+  
+def success
+end
+
+def logoff
+end
+  
+def already
+end
+  
+def success
+end
+
+def facebook
 end
